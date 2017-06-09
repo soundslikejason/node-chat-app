faea377eafcd46fd760656e30f95920b42223d70
Jason Leong <soundslikejason@users.noreply.github.com>
2017-06-09 16:21:31 -0700

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-  
-  socket.emit('createMessage', {
-    from: 'Jason',
-    text: 'Hey. This is good.'
-  });
 });
 
 socket.on('disconnect', function() {
