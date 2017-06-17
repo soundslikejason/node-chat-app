284df624a9813085fb8f9ccee62e789ff101c1ad
Jason Leong <soundslikejason@users.noreply.github.com>
2017-06-17 09:15:00 -0700

    printing message timestamps

--- playground/time.js
+++ playground/time.js
@@ -4,5 +4,9 @@ var date = moment();
 // date.add(100, 'year').subtract(9, 'months');
 console.log(date.format('MMM Do, YYYY'));
 
-var time = moment();
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var time = moment(createdAt);
 console.log(time.format('h:mm a'));
